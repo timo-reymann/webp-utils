ad8e6b4c53fd93e7b80ab3cd419993a206d3e982
Timo Reymann <mail@timo-reymann.de>
2020-12-03 23:40:03 +0100

    Add version in iso format, Add unknown default for GitSha

--- pkg/buildinfo/main.go
+++ pkg/buildinfo/main.go
@@ -3,7 +3,7 @@ package buildinfo
 import "time"
 
 // GitSha of the build
-var GitSha = "HEAD"
+var GitSha = "unknown"
 
 // Version contains the latest version tag
-var Version = time.Now().String()
+var Version = time.Now().UTC().Format("2006-01-02T15:04:05")
